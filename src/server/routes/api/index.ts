286ef2809459836a962186e16eb90120cf1c0ebb
staceylaurel <64053039+staceylaurel@users.noreply.github.com>
2020-08-28 16:14:12 -0500

    contact-donate

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -2,14 +2,10 @@ import {Router} from 'express';
 import blogsRouter from './blogs';
 import blogtagsRouter from './blogtags';
 import profileRouter from "./profile";
-import contactRouter from "./contact";
-import donateRouter from "./donate";
 
 const router = Router()
 
 router.use("/blogs", blogsRouter);
 router.use("/blogtags", blogtagsRouter);
 router.use("/profile", profileRouter);
-router.use("/donate", donateRouter);
-router.use("/contact", contactRouter);
 export default router;
